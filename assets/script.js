df683d0a9968d81017c14488e91ce59ab245fe2f
ZMAN <zachh@github.com>
2020-09-19 12:06:37 -0500

    added some java coding

---
+++ assets/script.js
@@ -0,0 +1,53 @@
+//numbers contianed in the array for password
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ];
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s" , "t", "u", "v", "w", "x", "y", "z" ];
+
+
+
+
+function generatepassword(){
+  var options
+}
+
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+
+
+function generatePassword() {
+  window.prompt("How many characters would you like your password to be?");
+    if()
+
+}
+
+
+
+
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+}
+
+
+ 
+  
+  
+  
+
+
+
+
+
+
+
+// Add event listener to generate button
+var generateBtn = addEventListener("click", writePassword);
+
+
