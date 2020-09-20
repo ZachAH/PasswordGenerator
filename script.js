2e0f5a50bef32e0f64d7534d6518ea4dc509c0a5
ZMAN <zachh@github.com>
2020-09-20 01:06:11 -0500

    final changes to js file

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-//numbers contianed in the array for password
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "?", ">", "<", "=", "+", "-",]
-
+//var for charcodes including alphabet letters, numbers 1-9, and special characters
+var numbers = [Math.floor(Math.random() * 10)];
+var lowercase = [String.fromCharCode(Math.floor(Math.random() * 26)+97)];
+var uppercase = [String.fromCharCode(Math.floor(Math.random() * 26)+65)];
+var special = [String.fromCharCode(Math.floor(Math.random() * 15)+33)];
+        //all the password prompts to create the password
 function getPasswordOptions() {
+    //if user types in letters it will turn them into a number string
     var length = parseInt(
         window.prompt("How many Characters Would You Like Your Password to Be?")
-    );
-
-    if (isNaN(length === true)) {
-        window.alert("Password must be Provided as a Number");
-        return;
-    }
+        );
+    //must be 8 or more characters to complete
     if (length < 8) {
         window.alert("Password must be at least 8 characters long.");
         return;
     }
+    //can't be more than 129 characters long
     if (length > 128) {
-        alert("Password lenght must be less than 129 characters");
+        window.alert("Password lenght must be less than 129 characters");
         return;
     }
+   //the next four window confirms are to select if they want or do not want the below characters in their password
     var hasSpecial = window.confirm(
         "click Ok to confirm including special characters."
     );
@@ -36,15 +35,17 @@ function getPasswordOptions() {
         "Click Ok to confirm including uppercase characters."
     );
 
+   //boolean argument for if they do not contain any of the characters the user will have to at least pick one to make a password succefully
     if (hasSpecial === false &&
         hasNumbers === false &&
         hasUppercase === false &&
-        hasLowercase === false 
+        hasLowercase === false
     ) {
         window.alert("Must Have at least one character type.");
         return;
     }
 
+    //boolean arguments for  using the above var arrays set up
     var passwordOptions = {
         length: length,
         hasNumbers: hasNumbers,
@@ -55,7 +56,7 @@ function getPasswordOptions() {
 
     return passwordOptions;
 }
-
+//using math.random and math.floor to use the arrays we have set up to convert to random numbers or characters based on the choices the user made
 function getRandom(arr) {
     var randIndex = Math.floor(Math.random() * arr.length);
     var randElement = arr[randIndex];
@@ -73,10 +74,13 @@ function getRandom(arr) {
 
 // Assignment code here
 function generatePassword() {
+    //any of the arrays the choose from
     var options = getPasswordOptions();
     var result = [];
     var possibleCharacters = [];
     var garanteedCharacters = [];
+    
+    //boolean arguments that are true grabbing one or more of the characters the user wants in there password grabbing them out of the arrays above
     if (options.hasSpecial) {
         possibleCharacters = possibleCharacters.concat(special);
         garanteedCharacters.push(getRandom(special));
@@ -98,6 +102,7 @@ function generatePassword() {
 
         result.push(possibleCharacter);
     }
+    // taking the var and adding 1 to the list of characters they choose containing the arrays set up and then taking the stored information and passing it to the password generator box
     for (var i = 0; i < garanteedCharacters.length; i++) {
         result[i] = garanteedCharacters[i];
     }
@@ -119,7 +124,7 @@ function generatePassword() {
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// final password will appear in the query with the id of #password 
 function writePassword() {
     var password = generatePassword();
     var passwordText = document.querySelector("#password");
@@ -128,5 +133,5 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+// on click the button will prompt user for password questions
 generateBtn.addEventListener("click", writePassword);
